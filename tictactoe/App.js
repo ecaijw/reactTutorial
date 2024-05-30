3c53724d0faa1f6dc81ac15a5cd4846f761fd7ad
luke <luke@d7d.com.au>
2024-05-30 16:12:39 +1000

    update the UI

--- tictactoe/App.js
+++ tictactoe/App.js
@@ -42,21 +42,12 @@ function Board({xIsNext, squares, onPlay}) {
             {status}
             <p></p>
         </div>
-        <div className='board-row'>
-            <Square value={squares[0]} onSquareClick={() => handleClick(0)}></Square>
-            <Square value={squares[1]} onSquareClick={() => handleClick(1)}></Square>
-            <Square value={squares[2]} onSquareClick={() => handleClick(2)}></Square>
-        </div>
-        <div className='board-row'>
-            <Square value={squares[3]} onSquareClick={() => handleClick(3)}></Square>
-            <Square value={squares[4]} onSquareClick={() => handleClick(4)}></Square>
-            <Square value={squares[5]} onSquareClick={() => handleClick(5)}></Square>
-        </div>
-        <div className='board-row'>
-            <Square value={squares[6]} onSquareClick={() => handleClick(6)}></Square>
-            <Square value={squares[7]} onSquareClick={() => handleClick(7)}></Square>
-            <Square value={squares[8]} onSquareClick={() => handleClick(8)}></Square>
-        </div>
+        {[0,1,2].map(row =>
+            <div className='board-row'>
+                {[row*3,row*3+1,row*3+2].map(v => <Square value={squares[v]} onSquareClick={() => handleClick(v)}></Square>)}
+            </div>
+
+        )}
         </>
     );
 }
@@ -83,6 +74,8 @@ function calculateWinner(squares) {
 }
 
 let App = function MyApp() {
+    console.log("App: render");
+
     const [history, setHistory] = useState([Array(9).fill(null)]);
     const [currentMove, setCurrentMove] = useState(0);
     const xIsNext = currentMove % 2 == 0;
@@ -92,10 +85,17 @@ let App = function MyApp() {
         const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
         setHistory(nextHistory);
         setCurrentMove(nextHistory.length - 1);
+
+        console.log("nextSquares: " + nextSquares);
+        console.log("nextHistory: " + nextHistory);
     }
 
     function jumpTo(nextMove) {
         setCurrentMove(nextMove);
+
+        // remove history
+        const nextHistory = history.slice(0, nextMove + 1);
+        setHistory(nextHistory);
     }
 
     const moves = history.map((square, move) => {
@@ -103,14 +103,25 @@ let App = function MyApp() {
         if (move > 0) {
             description = 'Go to move #' + move;
         } else {
-            description = 'Go to gamme start';
+            description = 'Go to game start';
         }
 
+        // last step should be clickable
+        let component;
+        if (move == history.length - 1) {
+            component =
+            <div key={move}>
+                {description}
+            </div>;
+        } else {
+            component =
+            <button onClick={() => jumpTo(move)}>
+                {description}
+            </button>;
+}
         return (
             <li key = {move}>
-                <button onClick={() => jumpTo(move)}>
-                    {description}
-                </button>
+                {component}
             </li>
         );
     });
